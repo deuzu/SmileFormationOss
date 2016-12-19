0a8ef01e4898cf22bbee0b5f7ff1d105b029e3e6
mehdi <mehdi.mkadem@yahoo.com>
2016-12-19 16:23:13 +0100

    UserManager and UsersRepository update1

---
+++ repository/UsersRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SmileOSS\Lab\OOP\Repository;
+
+use SmileOSS\Lab\OOP\Templating\Services\sqlDriverService;
+
+require_once "./services/sqlDriverService.php";
+
+class UsersRepository 
+{
+
+    public function userIsOk($login, $password) {
+        $dbh = getDatabaseConnection();
+
+        $password = sha1($password);
+
+        $query = 'SELECT login, password FROM users WHERE login="' . $login . '" and password="' . $password . '"';
+
+        $result = $dbh->query($query);
+
+        if ($result) {
+            return $result->fetchAll();
+        }
+        return false;
+    }
+
+    public function getAllUsers() {
+
+        $dbh = getDatabaseConnection();
+
+        $results = $dbh->query('SELECT * FROM users');
+
+        return $results->fetchAll();
+    }
+
+    public function getUserById($id) {
+
+        $dbh = getDatabaseConnection();
+
+        $results = $dbh->query('SELECT * FROM users WHERE ID=' . intval($id));
+
+        return $results->fetch();
+    }
+
+    /**
+     * get the role of a user from the table 'users' by passing the login 
+     * @param string $login
+     * @return array
+     */
+    public function getRoleByLogin($login) {
+        $dbh = getDatabaseConnection();
+        $rq = "SELECT role FROM users where login = '$login'";
+        $result = $dbh->query($rq);
+        return $result->fetch();
+    }
+
+}
