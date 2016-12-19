430859cb1b38588465f0eca43295ddf99df33d36
mounira <mounira.houmari@gmail.com>
2016-12-19 16:04:43 +0100

    formatage parentController.php

--- Controller/ParentController.php
+++ Controller/ParentController.php
@@ -4,20 +4,16 @@ namespace SmileOSS\Lab\OOP\Controller;
 
 class ParentController
 {    
-        
-     public function render($file, array $args = [])
+    public function render($file, array $args = [])
     {
-    
         if (!file_exists($file)) {
             throw new \Exception(sprintf('The file %s cannot be found', $file));
         }
 
         extract($args);
-
+        
         ob_start();
         include $file;
         ob_end_flush();
-  
-    } 
-   
+    }  
 }
