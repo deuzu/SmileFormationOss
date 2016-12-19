d91571f033081650fc932bc887e8860fa5b0cd5c
rimlab <labehri2000@yahoo.fr>
2016-12-19 15:52:52 +0100

    sign up
    Conflicts:
    	Controller/signUpController.php
    	controller/SignUpController.php
    	controller/signUpController.php

---
+++ controller/SignUpController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SmileOSS\Lab\OOP\Controller;
+
+require_once("./form/signUpForm.php");
+
+class SignUpController
+{
+    public function signUpAction(){
+        
+        If(isset($_POST['valider'])){
+
+            if(isset($_POST['lastName'])){ $userLastName  =  $_POST['lastName'] ;}
+            if(isset($_POST['firstName'])){ $userFirstName   = $_POST['firstName'];}
+            if(isset($_POST['password'])){  $userPsw =  $_POST['password'];}
+            if(isset($_POST['login'])){  $userLogin =  $_POST['login'];}
+            if(isset($_POST['password'])){ $userPhone  = $_POST['phone'];}
+            if(isset($_POST['email'])){ $userEmail  = $_POST['email'];}
+
+            //Appel de la fonction validateUser qui est définis dans le signUpForm
+            $errorForm = validateUser($userLastName,$userLogin,$userPsw);
+
+            if ($errorForm == false) {
+
+                //S'il n'ya pas d'erreurs sur le formulaire saisi, je crée un utilisateur et je le redirige vers la page du login avec une action=""
+                createUser($userLogin,$userPsw,'USER',$userFirstName,$userLastName,$userPhone,$userEmail);
+                header('Location:?controller=planning&action=list');
+
+            }
+
+
+        }
+        
+        TemplateEngine::render(__DIR__.'/../views/signUp/SignUp.php', ['errorForm' => $errorForm]);
+
+   
+    }
+}
