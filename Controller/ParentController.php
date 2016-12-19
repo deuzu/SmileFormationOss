7573253406e00a6d8033ad527fd705b1e90c98a3
mounira <mounira.houmari@gmail.com>
2016-12-19 16:03:03 +0100

    creation parentController.php

---
+++ Controller/ParentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SmileOSS\Lab\OOP\Controller;
+
+class ParentController
+{    
+        
+     public function render($file, array $args = [])
+    {
+    
+        if (!file_exists($file)) {
+            throw new \Exception(sprintf('The file %s cannot be found', $file));
+        }
+
+        extract($args);
+
+        ob_start();
+        include $file;
+        ob_end_flush();
+  
+    } 
+   
+}
