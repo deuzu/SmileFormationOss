84f64e4144deac9db31671cb1d8e9da170619295
Dihcar <edjek@hotmail.fr>
2016-12-20 11:22:13 +0100

    TRainersRepository POO

--- src/Repository/TrainersRepository.php
+++ src/Repository/TrainersRepository.php
@@ -1,26 +1,38 @@
 <?php
-
 namespace SmileOSS\Lab\OOP\Repository;
 
 use SmileOSS\Lab\OOP\Database\DatabaseManager;
 
-class PlanningRepository
+class TrainersRepository
 {
+    /**
+     * @var DatabaseManager
+     */
+    private $databaseManager;
+    /**
+     * @param DatabaseManager $databaseManager
+     */
+    
+    public function __construct(DatabaseManager $databaseManager)
+    {
+        $this->databaseManager = $databaseManager->getConnection();
+    }
+    
+    /**
+     * @return array
+     */
     public function getAllTrainers()
     {
-        $dbh = getDatabaseConnection();
-
-        $results = $dbh->query('SELECT * FROM trainers');
-
-        return $results->fetchAll();
+        return $this->databaseManager->query("SELECT * FROM trainers")->fetchAll();
     }
-
+    
+    /**
+     * @param int $id
+     *
+     * @return array
+     */
     public function getTrainerById($id)
     {
-        $dbh = getDatabaseConnection();
-
-        $results = $dbh->query('SELECT * FROM trainers WHERE ID='.intval($id));
-
-        return $results->fetch();
+        return $this->databaseManager->prepare("SELECT * FROM trainers WHERE date='.$id.'");
     }
 }
